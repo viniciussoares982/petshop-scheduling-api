854b5a91f19d1d7fffc43611a399a2f00d55d217
Vinicius Soares <1165546@sga.pucminas.br>
2023-08-15 19:26:37 -0300

    feat: creating user login auth route

--- src/app/Auth/controllers/AuthController.ts
+++ src/app/Auth/controllers/AuthController.ts
@@ -1,8 +1,21 @@
 import { Request, Response } from 'express'
+import AuthService from '@app/Auth/services/AuthService'
+import AuthError from '@app/Auth/exceptions/AuthError'
 
 class AuthController {
   async create(req: Request, res: Response): Promise<Response> {
-    return res.status(200).json({ message: 'hello world!' })
+    const { email, password } = req.body
+
+    try {
+      const { user, token } = await new AuthService().signIn(email, password)
+
+      return res.status(200).json({ user, token })
+    } catch (error) {
+      if (error instanceof AuthError) {
+        return res.status(401).send()
+      }
+      return res.status(500).json({ error })
+    }
   }
 
   async destroy() {
--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,8 @@ dotenv.config()
 
 export default {
   port: Number(process.env.PORT) || 3000,
+  auth: {
+    secret: process.env.AUTH_SECRET || 'secret',
+    expiresIn: process.env.AUTH_EXPIRES_IN || '7d',
+  },
 }
